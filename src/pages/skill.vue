2b373f69f20595900d1825d778a0d424c0e9c9e7
林珈誼 <joyce@linjiayideMacBook-Air.local>
2023-08-15 21:16:04 +0800

    skill

--- src/pages/skill.vue
+++ src/pages/skill.vue
@@ -3,9 +3,13 @@ import circleProgress from "@/components/circleProgress.vue";
 import lineProgress from "@/components/lineProgress.vue";
 import { onMounted, onBeforeUnmount, ref } from 'vue';
 import VanillaTilt from 'vanilla-tilt';
+import { deviceStore } from '@/stores/device.js';
+const store = deviceStore()
+
 const vueChart =  ref(null);
 const nuxtChart =  ref(null);
 const frontendChart =  ref(null);
+//----- 原型進度條的動畫
 const vuePercent = ref(0);
 const nuxtPercent = ref(0);
 const vueMax = 75;
@@ -18,7 +22,10 @@ const timer = setInterval(() => {
 onBeforeUnmount(() => {
   clearInterval(timer);
 });
+//--------------------
 onMounted(() => {
+  console.log(store.deviceIsMobile);
+  if (store.deviceIsMobile) return;
   VanillaTilt.init(vueChart.value, { max: 25, speed: 400 });
   VanillaTilt.init(nuxtChart.value, { max: 25, speed: 400 });
   VanillaTilt.init(frontendChart.value, { max: 25, speed: 400 });
@@ -58,6 +65,8 @@ const logoObj = {
         .tag(v-for='item of ["Nuxt.js", "SPA", "SSR", "SEO"]' :key='item') {{ `# ${item}` }}
   .frontend-chart(ref="frontendChart")
     img.logo(:src="logoObj.frontend")
+    .desc-area 
+      .desc HTML、CSS、JavaScript、Pug和Sass是用於建立和設計網頁結構、外觀、互動和動態效果的技術，結合使用可以創建出美觀、互動性強且功能豐富的網頁應用和互動式網站。Git用於追蹤和管理程式碼的變更，促進團隊協作和版本管理。
     .skill-process
       .process-area
         .col
@@ -110,6 +119,16 @@ const logoObj = {
   padding: 20px;
   grid-template-columns: 1fr 1fr;
   grid-template-areas: "vue nuxt" "frontend frontend";
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-areas: "vue" "nuxt" "frontend";
+    gap: 50px;
+    margin: 100px 10px;
+    // padding: 10px;
+
+
+    // grid-template-areas: "vue nuxt" "frontend frontend";
+  }
   .card {
     position: relative;
     // width: 100%;
@@ -117,6 +136,13 @@ const logoObj = {
     padding: 20px;
     border-radius: 20px;
     background: linear-gradient(160deg, #907d83b7, #20288587);
+    @media (max-width: 768px) {
+      // margin: 0 50px;
+      &:hover {
+        box-shadow: none;
+      } 
+
+    }
     &:hover {
       box-shadow: 0 0 10px 0px #f9f9f9;
       .logo {
@@ -132,6 +158,13 @@ const logoObj = {
       top: -40px;
       left: -40px;
       transition: all 0.3s ease-in-out;
+      @media (max-width: 768px) {
+        transform: scale(1);
+        top: -20px;
+        left: -10px;
+        width: 50px;
+        height: 50px;
+      }
     }
   }
   .desc-area {
@@ -169,6 +202,11 @@ const logoObj = {
     justify-content: space-between;
     align-items: center;
     gap: 20px;
+    @media (max-width: 768px) {
+      grid-template-columns: 1fr;
+      grid-template-rows: 100px 1fr;
+    
+    }
     
   }
   .nuxt-chart {
@@ -179,19 +217,28 @@ const logoObj = {
     justify-content: space-between;
     align-items: center;
     gap: 20px;
-    // background-color: rgb(30, 163, 106);
-
+    @media (max-width: 768px) {
+      grid-template-columns: 1fr;
+      grid-template-rows: 100px 1fr;
+    
+    }
   }
   .frontend-chart {
     @extend .card;
     grid-area: frontend;
     padding: 40px 40px;
+    @media (max-width: 768px) {
+      padding: 40px 20px 20px 20px;
+    }
     // background-color: rgb(234, 227, 23);
     .skill-process {
       display: grid;
       grid-template-columns: 1fr 1fr;
       gap: 80px;
-
+      @media (max-width: 768px) {
+        grid-template-columns: 1fr;
+        gap: 0;
+      }
       .process-area {
         .col {
           display: flex;
@@ -209,6 +256,10 @@ const logoObj = {
   .f-logo {
     width: 50px;
     height: 50px;
+    @media (max-width: 768px) {
+      width: 25px;
+      height: 25px;
+    }
   }
 
 }
